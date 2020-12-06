a7bfcee43f4457f5d3224143ff55d26cc93a8cbd
Martin Feckie <mfeckie@gmail.com>
2020-12-06 14:50:11 +0800

    Remove console log

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ module.exports = {
   description: "Generate an ember-cli addon using TypeScript",
   name: "ember-typescript-addon-blueprint",
   locals(options) {
-    console.log(options);
     return {
       name: options.entity.name
     }
